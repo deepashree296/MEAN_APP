6fe769cec98bb0fdaf9d9a7f86a12449897e7333
deepashree296 <kediadeepashree@gmail.com>
2017-06-28 17:07:39 +0530

    simple app

---
+++ server.js
@@ -0,0 +1,11 @@
+// main application file
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('./config/express');
+var app = express();
+app.listen(3000);
+
+module.exports = app;
+
+console.log('server running at http://localhost:3000');
