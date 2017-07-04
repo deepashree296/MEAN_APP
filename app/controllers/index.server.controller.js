1de1738baf2c54ac1e51950690a0a450ba4243c7
deepashree296 <kediadeepashree@gmail.com>
2017-07-04 09:23:20 +0530

    session

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,3 +1,13 @@
 exports.render = function(req, res) {
-	res.send("Hello World");	
+
+	if(req.session.lastVisit) {
+		console.log('I last visited at ' + req.session.lastVisit);
+	}
+
+	req.session.lastVisit = new Date();
+
+	//res.send("Hello World");	
+	res.render('index', {
+		title: "Hello World"
+	});
 };
--- config/express.js
+++ config/express.js
@@ -1,10 +1,12 @@
 // express configuration file
 
+var config = require('./config');
 var express = require('express');
 var morgan = require('morgan');
 var compress = require('compression');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-overrride');
+var methodOverride = require('method-override');
+var session = require('express-session');
 
 module.exports = function() {
 	var app = express();
@@ -21,10 +23,18 @@ module.exports = function() {
 
 	app.use(methodOverride());
 
-	app.set('views' , '../app/views');
+	app.use(session({
+		saveUninitialized: true,
+		resave: true,
+		secret: config.sessionSecret
+	}));
+
+	app.set('views' , './app/views');
 	app.set('view engine' , 'ejs');
 
 
 	require('../app/routes/index.server.routes') (app);
+
+	app.use(express.static('./public'));
 	return app;
 };
