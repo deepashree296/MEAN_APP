ee75acde939de5dfc6a34d721fa5330a631f35e9
deepashree296 <kediadeepashree@gmail.com>
2017-07-07 16:09:53 +0530

    passport authentication

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,12 +1,28 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
-	email: String,
-	username: String,
-	password: String
+	email: {
+		type: String,
+		match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
+	},
+	username: {
+		type: String,
+		unique: true,
+		required: 'username is required',
+		trim: true
+	},
+	password: {
+		type: String,
+		validate: [
+		function(password) {
+			return password && password.length > 6;
+		}
+		]
+	}
 
 });
 
--- config/express.js
+++ config/express.js
@@ -7,6 +7,7 @@ var compress = require('compression');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var session = require('express-session');
+var passport = require('passport');
 
 module.exports = function() {
 	var app = express();
@@ -31,6 +32,9 @@ module.exports = function() {
 
 	app.set('views' , './app/views');
 	app.set('view engine' , 'ejs');
+
+	app.use(passport.initialize());
+ 	app.use(passport.session());
 
 
 	require('../app/routes/index.server.routes') (app);
